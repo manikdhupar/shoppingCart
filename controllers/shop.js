6b9176c2943d0fd0a3ac8d91e2841547a5f7b170
manik dhupar <manik.dhupar7@gmail.com>
2019-03-09 23:02:22 +0530

    updated getCart

--- controllers/shop.js
+++ controllers/shop.js
@@ -45,9 +45,11 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  return req.user
-    .getCart()
-    .then(products => {
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
