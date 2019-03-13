604ee93925fd31ff046f56a6962592ec16df569d
manik dhupar <manik.dhupar7@gmail.com>
2019-03-14 01:05:37 +0530

    added postNewPassword route

--- controllers/auth.js
+++ controllers/auth.js
@@ -186,3 +186,32 @@ exports.getNewPassword = (req, res, next) => {
       console.log(err);
     });
 };
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId
+  })
+    .then(user => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then(hashedPassword => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = null;
+      resetUser.resetTokenExpiration = null;
+      return resetUser.save();
+    })
+    .then(() => {
+      res.redirect('/login');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
